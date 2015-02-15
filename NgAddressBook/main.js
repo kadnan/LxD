fdf3029a2ae3a74354ab3a433611e74dfadcd288
Adnan Siddiqi <kadnan@gmail.com>
2015-02-15 18:48:45 +0500

    Implementing templates and routers

--- NgAddressBook/main.js
+++ NgAddressBook/main.js
@@ -1,13 +1,42 @@
 
-angular.module('ngAddressBook', [])
+var ngAddressBook = angular.module('ngAddressBook', ['ngRoute']);
+
+ngAddressBook.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'views/list.html',
+                controller: 'ngAddressListController'
+            }).
+            when('/add', {
+                templateUrl: 'views/add.html',
+                controller: 'ngAddressListController'
+            }).
+            when('/details/:id', {
+                templateUrl: 'views/details.html',
+                controller: 'ngAddressDetailsController'
+            });
+    }]);
 // add a controller to it
-    .controller('ngAddressListController', ['$scope', function ($scope)
+ngAddressBook.controller('ngAddressListController', ['$scope', function ($scope)
     {
         $scope.contacts = [
-            {first_name:'Jane', last_name:'Doe','Phone':'123-456789','Email':'jane@example.com'},
-            {first_name:'Jhon', last_name:'Doe','Phone':'123-456789','Email':'jhon@example.com'}
+            {id:1,first_name:'Jane', last_name:'Doe','Phone':'123-456789','Email':'jane@example.com'},
+            {id:2,first_name:'Jhon', last_name:'Doe','Phone':'123-456789','Email':'jhon@example.com'}
         ];
         $scope.getTotalContacts = function () {
             return $scope.contacts.length;
         };
     }]);
+
+
+
+    ngAddressBook.controller('ngAddressDetailsController', ['$scope', '$routeParams',
+    function($scope, $routeParams)
+    {
+        $scope.id = $routeParams;
+        //alert($routeParams);
+    }]);
+
+
+
